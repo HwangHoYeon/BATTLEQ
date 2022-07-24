dcda41e63d16c6aea07bca548446b7db3d0eeee3
rhtld123 <rhtld123@gmail.com>
2022-07-25 00:32:54 +0900

    BQ-44 [feat] : BE - quiz search 리팩토링

--- backend/src/main/java/com/battleq/quiz/repository/QuizRepository.java
+++ backend/src/main/java/com/battleq/quiz/repository/QuizRepository.java
@@ -2,21 +2,49 @@ package com.battleq.quiz.repository;
 
 
 import com.battleq.quiz.domain.entity.Quiz;
+import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface QuizRepository extends CrudRepository<Quiz, Long> {
 
     public Optional<Quiz> findById(Long id);
+
     public Slice<Quiz> findAll(Pageable pageable);
+
     public Slice<Quiz> findByUserId(Long userId, Pageable pageable);
+
+    @Query(value =
+        "SELECT q "
+            + "FROM Quiz q JOIN q.user m "
+            + "WHERE m.nickname = :nickname "
+        , countQuery =
+        "SELECT COUNT(q) "
+            + "FROM Quiz q JOIN q.user m "
+            + "WHERE m.nickname = :nickname")
+    public Page<Quiz> findByMemberNickname(String nickname, Pageable pageable);
+
+    @Query(value =
+        "SELECT q "
+            + "FROM Quiz q "
+            + "WHERE q.name LIKE %:name% "
+        , countQuery =
+        "SELECT COUNT(q) "
+            + "FROM Quiz q "
+            + "WHERE q.name LIKE %:name% ")
+    public Page<Quiz> findByName(String name, Pageable pageable);
+
+    @Query(value = "SELECT q "
+        + "FROM Quiz q "
+        + "WHERE q.category LIKE %:category% ",
+        countQuery = "SELECT COUNT(q) "
+            + "FROM Quiz q "
+            + "WHERE q.category LIKE %:category% ")
+    public Page<Quiz> findByCategory(String category, Pageable pageable);
 
 }
